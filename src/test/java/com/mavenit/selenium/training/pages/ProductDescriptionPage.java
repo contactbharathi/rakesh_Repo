e9488a7372338351f538b11b13871e5538e581d6
rakesh-reddy <rakeshreddy255@gmail.com>
2019-03-09 14:04:41 +0000

    fixing basket test

--- src/test/java/com/mavenit/selenium/training/pages/ProductDescriptionPage.java
+++ src/test/java/com/mavenit/selenium/training/pages/ProductDescriptionPage.java
@@ -9,7 +9,7 @@ public class ProductDescriptionPage extends DriverManager {
     @FindBy(xpath = "//div[4]/div[2]/button/span/span")
     private WebElement trolleyBtn;
 
-    @FindBy(xpath = "//a[contains(text(),'Go to kdafhisudhfsdhf')]")
+    @FindBy(xpath = "//a[contains(text(),'Go to Trolley')]")
     private WebElement goToTrolleyBtn;
 
     public void addToBasket() {
